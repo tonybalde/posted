afa863c74cc9a333cfd52f7ce2ba3e43c159c727
Tony Baldessari <tonybalde@hotmail.com>
2024-01-08 09:44:04 -0300

    Register form working

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:4000/register", {
+        method: "POST",
+        body: JSON.stringify({username,password}),
+        headers: {'Content-Type':'application/json'},
+      });
+      if(response.status === 200) {
+        alert("Registration Successful");
+      } else {
+        alert("Registration Failed")
+      }
+  }
+  
   return (
     <div>
-      <form className="register">
+      <form className="register" onSubmit={register} >
       <h1>Register</h1>
-        <input type='text' placeholder='username'/>
-        <input type='password' placeholder='password'/>
+        <input type='text' 
+          placeholder='username' 
+          value={username}
+          onChange={ev => setUsername(ev.target.value)}  
+          />
+        <input type='password' 
+          placeholder='password' 
+          value={password}
+          onChange={ev => setPassword(ev.target.value)}  
+          />
         <button>Register</button>
       </form>
     </div>
